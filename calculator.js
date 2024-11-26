fd402df8a9b0b33d6f89a11e98fb76f190d3ae1c
ronsela209 <186534644+ronsela209@users.noreply.github.com>
2024-11-26 19:33:10 +0700

    Calculator Completed, before making it prettier

--- calculator.js
+++ calculator.js
@@ -1,5 +1,114 @@
+let calculatorElements = document.querySelectorAll('.num');
+let signElements = document.querySelectorAll('.sign');
+
+let clickedDisplay = document.getElementById('clicked-display');
+let clear = document.getElementById('clear');
+let equal = document.getElementById('equal')
+let firstValue = "";
+let operatorValue = "";
+let operatorCount = 0;
+let secondValue = "";
+let deleteBtn = document.getElementById('delete');
+
+deleteBtn.addEventListener('click' , () => {
+
+    if((firstValue) && (secondValue)){
+        secondValue = secondValue.slice(0,-1)
+        clickedDisplay.textContent = secondValue;
+    }
+    else if((firstValue) && operatorValue === ""){
+        firstValue = firstValue.slice(0,-1)
+        clickedDisplay.textContent = firstValue;      
+
+    }
+    
+})
+
+clear.addEventListener('click', clearInput);
+equal.addEventListener('click', () => {
+
+    if((firstValue) && (secondValue) && (secondValue !== "0") && operatorCount >= 0){
+
+        operate(parseFloat(firstValue), operatorValue, parseFloat(secondValue));
+        
+    }
+    else if(secondValue === "0"){
+
+        alert ("Cant divide in 0,calculator values cleared");
+        clearInput();
+    }
+    else{
+
+        alert("Please use the calculator as 2 numbers and operator")
+        clearInput();
+    }
+})
+
+
+calculatorElements.forEach(element => {
+    element.addEventListener('click', ()=> {
+        console.log(element)
+        
+        
+        if(operatorCount===0){
+            if(element.textContent === "." && firstValue.includes(".")){
+                return;
+
+            }
+
+            firstValue += element.textContent
+            clickedDisplay.textContent += element.textContent;
+             console.log("first value= " + firstValue);
+        }
+        else if(operatorCount >= 1){
+            if(element.textContent === "." && secondValue.includes(".")){
+                return;
+
+            }
+            console.log("operator bigger than 1")
+            if(!secondValue){
+                console.log("secondValue");
+                clickedDisplay.textContent = "";
+                clickedDisplay.textContent += element.textContent;
+                secondValue += element.textContent;
+
+
+            }
+            else{
+                console.log(secondValue);
+                secondValue += element.textContent;
+                clickedDisplay.textContent += element.textContent;
+
+            }
+             console.log("second value= " + secondValue);
+            }     
+        
+        }
+    )
+});
+
+
+signElements.forEach(sign => {
+    sign.addEventListener('click', () => {
+        if((firstValue) && (secondValue) && (secondValue !== "0") && operatorCount >= 0) {
+        
+        firstValue = operate(parseFloat(firstValue), operatorValue, parseFloat(secondValue))
+        secondValue = "";
+            }
+            else if(secondValue === "0"){
+
+                alert ("Cant divide in 0,calculator values cleared");
+                clearInput();
+            }
+        operatorValue = sign.textContent;
+        console.log("operator value= " + operatorValue);
+
+     operatorCount++;    
+    })
+})
 function add(firstNum , secondNum){
     let result = firstNum + secondNum;
+    
     console.log("add result= " + result)
     return result;
     }
@@ -26,36 +135,74 @@ function divide(firstNum , secondNum){
     }
 
     function operate(firstNum, operator, secondNum){
-
+        let operateResult = ""
         if(operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/'){
-
+            
             return "Unexaptable operator";
         }
         else{
-
+            console.log("equal function")
             switch(operator){
                 case '+' :
-                    add(firstNum, secondNum)
-                    break;
+                    
+                    operateResult =  add(firstNum, secondNum);
+                    clickedDisplay.textContent = Math.floor(operateResult * 100) / 100;
+                    return operateResult
+                    
                 case '-' :
-                    substract(firstNum, secondNum)
-                    break;
-                case '+' :
-                    multiply(firstNum, secondNum)
-                    break;
+                    operateResult = substract(firstNum, secondNum)
+                    clickedDisplay.textContent = Math.floor(operateResult * 100) / 100;
+                    return operateResult
+                    
+                case '*' :
+                    operateResult = multiply(firstNum, secondNum)
+                    clickedDisplay.textContent = Math.floor(operateResult * 100) / 100;
+                    return operateResult
+                    
                 case '/' :
-                    divide(firstNum, secondNum)
-                    break;
+                    operateResult = divide(firstNum, secondNum)
+                    clickedDisplay.textContent = Math.floor(operateResult * 100) / 100;
+                    return operateResult
+
+                    
 
 
             }
         }
 
     }
+function clearInput(){
+     clickesCounter = 0;
+     firstValue = "";
+     operatorValue = "";
+     secondValue = "";
+     operatorCount = 0;    
+     clickedDisplay.textContent = "";
 
-//let testOne = parseInt(prompt("Enter first Num"));
-//let testSecond = parseInt(prompt("Enter second Num"));
-//add(testOne, testSecond);
-//substract(testOne, testSecond);
-//multiply(testOne, testSecond);
-//divide(testOne, testSecond);
+}
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key; // e.g., "1", "+", "Enter"
+    calculatorElements.forEach(element => {
+
+        if(element.textContent === key){
+            element.click();
+        }
+
+    })
+    signElements.forEach(element => {
+
+        if(element.textContent === key){
+            element.click();
+        }
+
+    })
+    if(key === 'Enter'){
+
+        equal.click()
+    }
+    // Find the button with a matching attribute or class
+    
+    
+});
+
